78d22eaab672816cb08ffe890d158775cacc9a50
Atindra Mardikar <atindramardikar@gmail.com>
2016-03-26 23:06:57 -0400

    Added scheduling events for particular users

--- Public/project/client/views/createEvent/invite.controller.js
+++ Public/project/client/views/createEvent/invite.controller.js
@@ -4,11 +4,19 @@
     angular
         .module("EventSchedulerApp")
         .controller("InviteController",InviteController);
-    function InviteController($rootScope, $location){
+    function InviteController($rootScope, $location, EventService){
         var vm=this;
-        vm.update= function(){
+        vm.createEvent= function(){
+            if($rootScope.currentUser==null){
+                EventService.
+                    createEvent($rootScope.event);
+            }
+            else{
+                EventService
+                    .createEventForUser($rootScope.currentUser._id,$rootScope.event);
+            }
             $rootScope.event=null;
-            $location.url("/myEvents");
+            $location.url("/home");
         }
     }
 })();
