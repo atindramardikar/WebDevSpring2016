107a53946b501c78fa9dea651ee5cc5168778eae
Atindra Mardikar <atindramardikar@gmail.com>
2017-10-09 13:11:59 -0400

    Update app.js

--- Public/assignment/server/app.js
+++ Public/assignment/server/app.js
@@ -3,7 +3,7 @@ module.exports = function(app, db, mongoose) {
     var formmodel = require("./models/form.model.js")(db,mongoose);
     var fieldModel = require("./models/field.model.js") (db,mongoose,formmodel);
 
-    var userservice = require("./services/user.service.server.js")(app, usermodel);
-    var formservice = require("./services/form.service.server.js")(app, formmodel);
-    var fieldService = require("./services/field.service.server.js") (app, formmodel, fieldModel);
+    //var userservice = require("./services/user.service.server.js")(app, usermodel);
+    //var formservice = require("./services/form.service.server.js")(app, formmodel);
+    //var fieldService = require("./services/field.service.server.js") (app, formmodel, fieldModel);
 }
